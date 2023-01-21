c61d85ba06913153b69ea8ecc5fae4d435583e92
anshulghogre4 <anshulghogre4@gmail.com>
2023-01-21 22:45:30 +0530

    added Register & login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import LandingPage from './Components/LandingPage/LandingPage';
+import {Routes,Route} from "react-router-dom"
 import './App.css';
+import Login from './Components/Registration/Login';
+import Register from './Components/Registration/Register';
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage/>
-    </div>
+    <Routes>
+      <Route path="/" element = {<LandingPage/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Register/>}/>
+      </Routes>
   );
 }
 
---
+++ client/src/Components/Registration/Register.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import {NavLink} from "react-router-dom"
+
+const Register = () => {
+  return (
+    <>
+    <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="text-center text-2xl font-bold">Sign up</div>
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" action="#" method="POST">
+          <div>
+            <label
+              htmlFor="firstName"
+              className="block text-sm font-medium text-gray-700"
+            >
+              First Name
+            </label>
+            <div className="mt-1">
+              <input
+                id="firstName"
+                name="firstName"
+                type="text"
+                autoComplete="firstName"
+               
+                required
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="lastName"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Last Name
+            </label>
+            <div className="mt-1">
+              <input
+                id="lastName"
+                name="lastName"
+                type="text"
+                autoComplete="lastName"
+               
+                required
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email address
+            </label>
+            <div className="mt-1">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+               
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <div className="mt-1">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                
+                required
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"  
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign up
+            </button>
+          </div>
+        </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="" />
+                <NavLink to={"/login"} className="block text-sm font-medium text-gray-700">
+                  Already having an account?
+                </NavLink >
+            </div>
+
+            
+          </div>
+         
+        </div>
+      </div>
+    </div>
+  </div>
+</>
+  )
+}
+
+export default Register
