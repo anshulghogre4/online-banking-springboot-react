2eed6387df48a4516ea62179d1f2d5bb645a420d
anshulghogre4 <anshulghogre4@gmail.com>
2023-01-23 21:36:10 +0530

    started with springboot

---
+++ server/onlinebanking/src/main/java/bankproject/onlinebanking/OnlinebankingApplication.java
@@ -0,0 +1,13 @@
+package bankproject.onlinebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebankingApplication.class, args);
+	}
+
+}
---
+++ server/onlinebanking/src/test/java/bankproject/onlinebanking/OnlinebankingApplicationTests.java
@@ -0,0 +1,13 @@
+package bankproject.onlinebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
