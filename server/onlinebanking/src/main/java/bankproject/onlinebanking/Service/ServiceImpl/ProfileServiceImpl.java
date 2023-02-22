9d46de124f0f61fc64be24db07e50a0231de9dcc
anshulghogre4 <anshulghogre4@gmail.com>
2023-02-22 19:13:24 +0530

    added profile implementation linked it with jpa

--- server/onlinebanking/src/main/java/bankproject/onlinebanking/Service/ServiceImpl/ProfileServiceImpl.java
+++ server/onlinebanking/src/main/java/bankproject/onlinebanking/Service/ServiceImpl/ProfileServiceImpl.java
@@ -1,14 +1,20 @@
 package bankproject.onlinebanking.Service.ServiceImpl;
 
 import bankproject.onlinebanking.Model.User;
+import bankproject.onlinebanking.Repository.UserRepository;
 import bankproject.onlinebanking.Service.ProfileService;
 
 public class ProfileServiceImpl implements ProfileService {
 
+    private UserRepository userRepo;
+
     @Override
     public User updateUser(User user) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
+
+        if (userRepo.existsById(user.getUserid())) {
+            userRepo.save(user);
+        }
+        return user;
     }
 
 }
