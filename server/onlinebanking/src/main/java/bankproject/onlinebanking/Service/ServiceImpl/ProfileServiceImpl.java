23ce8705172027435b328ff2db0f7ec9cf53e44a
anshulghogre4 <anshulghogre4@gmail.com>
2023-02-22 16:34:56 +0530

    implemented user services

---
+++ server/onlinebanking/src/main/java/bankproject/onlinebanking/Service/ServiceImpl/ProfileServiceImpl.java
@@ -0,0 +1,14 @@
+package bankproject.onlinebanking.Service.ServiceImpl;
+
+import bankproject.onlinebanking.Model.User;
+import bankproject.onlinebanking.Service.ProfileService;
+
+public class ProfileServiceImpl implements ProfileService {
+
+    @Override
+    public User updateUser(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
+    }
+
+}
---
+++ server/onlinebanking/src/main/java/bankproject/onlinebanking/Service/ServiceImpl/SignUpServiceImpl.java
@@ -0,0 +1,29 @@
+package bankproject.onlinebanking.Service.ServiceImpl;
+
+import java.util.List;
+import java.util.UUID;
+
+import bankproject.onlinebanking.Model.User;
+import bankproject.onlinebanking.Service.SignUpService;
+
+public class SignUpServiceImpl implements SignUpService {
+
+    @Override
+    public User createUser(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+
+    @Override
+    public User getAUser(UUID userId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAUser'");
+    }
+
+    @Override
+    public List<User> GetAllUsers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'GetAllUsers'");
+    }
+
+}
