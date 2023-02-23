f7280d24517b425334c38fb5afdb6025a23a656c
AniketSG-22 <aniketghogare2017@gmil.com>
2023-02-23 17:23:37 +0530

    updated bankaccount and transaction repository

--- server/onlinebanking/src/main/java/bankproject/onlinebanking/Repository/TransactionRepository.java
+++ server/onlinebanking/src/main/java/bankproject/onlinebanking/Repository/TransactionRepository.java
@@ -1,5 +1,7 @@
 package bankproject.onlinebanking.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,13 @@ import bankproject.onlinebanking.Model.Transactions;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transactions, Integer> {
+
+    public List<Transactions> getDetailsByAccountNo(Long accountno);
+
+    public Transactions getCurrentTransaction(long fromAccount);
+
+    public Transactions getTransactionsByFromAccount(Long accountno);
+
+    public List<Transactions> getTransactionsByToAccount(long toAccount);
 
 }
