6c8b47801ccfa30a5c2c3c223b1a9a7d0022831f
AniketSG-22 <aniketghogare2017@gmil.com>
2023-02-23 17:09:08 +0530

    Transaction services implementation added

---
+++ server/onlinebanking/src/main/java/bankproject/onlinebanking/Service/ServiceImpl/FundTransferServiceImpl.java
@@ -0,0 +1,21 @@
+package bankproject.onlinebanking.Service.ServiceImpl;
+
+import bankproject.onlinebanking.Model.BankAccount;
+import bankproject.onlinebanking.Model.Transactions;
+import bankproject.onlinebanking.Service.FundTransferService;
+
+public class FundTransferServiceImpl implements FundTransferService{
+
+    @Override
+    public Transactions save(long accounNo, String name, String desctription, double amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public BankAccount updateFundDeducion(BankAccount bankAccount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateFundDeducion'");
+    }
+    
+}
