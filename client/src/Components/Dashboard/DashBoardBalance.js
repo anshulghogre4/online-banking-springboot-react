07c3972ba57746c9e5cbd9426c59c3d861e04357
AniketSG-22 <aniketghogare2017@gmil.com>
2023-03-06 11:28:28 +0530

    Dashboard updated

--- client/src/Components/Dashboard/DashBoardBalance.js
+++ client/src/Components/Dashboard/DashBoardBalance.js
@@ -1,41 +1,74 @@
-import React from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import NavbarDashboard from './NavbarDashboard'
+import axios from "../Utills/AxiosWithJWT.js"
+import { useBankingSystem } from '../Context/UserContext'
+import { toast } from 'react-hot-toast'
 
 
 const DashBoardBalance = () => {
+
+
+  const { BASE_URL, userDetails, setUser: setUserDetails } = useBankingSystem();
+  const [balance, setBalance] = useState(0);
+
+  console.log(userDetails?.userId);
+  console.log(userDetails.accounts[0].accountno);
+
+  let userName = userDetails.firstname + " " + userDetails.lastname;
+  let accountNo = userDetails.accounts[0].accountno;
+  // setBalance(userDetails.accounts[0].balance);
+
+
+  const checkbal = async (e) => {
+
+    try {
+
+      const resp = await axios.get(`${BASE_URL}/account/checkbal/${accountNo}`);
+
+      setBalance(resp.data[0].balance);
+      console.log(resp);
+    }
+    catch { }
+  }
+
+  useEffect(() => {
+    console.log("Welcome to useeffect!");
+    checkbal();
+}, []);
+
   return (
     <div>
-        <NavbarDashboard/>
-        <section className='h-[80vh] bg-gray-600 border pt-[2rem]'>
+      <NavbarDashboard />
+      <section className='h-[80vh] bg-gray-600 border pt-[2rem]'>
 
-<h2 className='text-[1.5rem] text-[#f1f2f6] font-semibold text-center'>DashBoard</h2>
-<div className=' flex flex-row justify-around items-center'>
+        <h2 className='text-[1.5rem] text-[#f1f2f6] font-semibold text-center'>DashBoard</h2>
+        <div className=' flex flex-row justify-around items-center'>
 
-<div className=' flex flex-col justify-center items-center gap-4'>
-      <h3 className='text-[1.2rem] text-[#f1f2f6]'>Operations</h3>
-      <NavLink to={"/dashboard/balance"} ><button className='w-[10rem] p-2 bg-[#f1f2f6] font-semibold rounded '>Check balance</button></NavLink>
-      <NavLink to={"/dashboard/trx"} ><button className='w-[10rem] p-2 bg-[#f1f2f6] font-semibold rounded '>Transfer Amount</button></NavLink>
-      <NavLink to={"/dashboard/Stmt"} ><button className='w-[10rem] p-2 bg-[#f1f2f6] font-semibold rounded '>Statements</button></NavLink>
+          <div className=' flex flex-col justify-center items-center gap-4'>
+            <h3 className='text-[1.2rem] text-[#f1f2f6]'>Operations</h3>
+            <NavLink to={"/dashboard/balance"} ><button className='w-[10rem] p-2 bg-[#f1f2f6] font-semibold rounded ' onClick={checkbal}>Check balance</button></NavLink>
+            <NavLink to={"/dashboard/trx"} ><button className='w-[10rem] p-2 bg-[#f1f2f6] font-semibold rounded '>Transfer Amount</button></NavLink>
+            <NavLink to={"/dashboard/Stmt"} ><button className='w-[10rem] p-2 bg-[#f1f2f6] font-semibold rounded '>Statements</button></NavLink>
 
-</div>
-
-
+          </div>
 
 
-<div className='w-[50rem]'>
-   <h2 className='text-[2rem] text-[#f1f2f6] w-[50rem]'>Available Balance</h2>
-   <div className='flex  flex-col  justify-center items-start gap-2 mt-[3rem] text-[#f1f2f6]' >
-        <h3>Name:</h3>
-        <h3>AccountNo:</h3>
-        <h3>Balance:</h3>
 
 
-   </div>
+          <div className='w-[50rem]'>
+            <h2 className='text-[2rem] text-[#f1f2f6] w-[50rem]'>Available Balance</h2>
+            <div className='flex  flex-col  justify-center items-start gap-2 mt-[3rem] text-[#f1f2f6]' >
+              <h3>Name: {userName}</h3>
+              <h3>AccountNo: {accountNo}</h3>
+              <h3>Balance: {balance}</h3>
 
-</div>
-</div>
-</section>
+
+            </div>
+
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
