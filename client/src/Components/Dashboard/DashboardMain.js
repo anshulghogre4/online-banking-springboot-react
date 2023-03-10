ab261df35656d513d7bc68570e789e29eff292b7
anshulghogre4 <anshulghogre4@gmail.com>
2023-03-10 21:45:01 +0530

    added hidining function

--- client/src/Components/Dashboard/DashboardMain.js
+++ client/src/Components/Dashboard/DashboardMain.js
@@ -11,9 +11,10 @@ import { useNavigate, NavLink } from 'react-router-dom'
 const DashboardMain = () => {
 
   const navigateTo = useNavigate();
-  const { BASE_URL, userDetails, setUser: setUserDetails } = useBankingSystem();
+  const { BASE_URL, userDetails, setUser: setUserDetails, gettingAUser } = useBankingSystem();
 
   useEffect(() => {
+    gettingAUser();
     if(!sessionStorage.getItem("jwtToken")){
       navigateTo("/")
     }
@@ -79,43 +80,8 @@ const DashboardMain = () => {
               Click here to apply for Account Oppening!
             </button>
 
-            {/* Modal Testing */}
-            <div>
-              {/* <Button bg-white
-   onClick={()=> setShow(!show)}>
-     Click here to apply for Account Oppening!
-  </Button>
-  <Modal
-    show={show}
-    size="md"
-    popup={true}
-    onClose={()=> setShow(!show)}
-  >
-    <Modal.Header />
-    <Modal.Body>
-      <div className="text-center">
-        <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
-        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-          Are you sure you want to delete this product?
-        </h3>
-        <div className="flex justify-center gap-4">
-          <Button
-            color="failure"
-            onClick={()=> setShow(!show)}
-          >
-            Yes, I'm sure
-          </Button>
-          <Button
-            color="gray"
-            onClick={()=> setShow(!show)}
-          >
-            No, cancel
-          </Button>
-        </div>
-      </div>
-    </Modal.Body>
-  </Modal> */}
-            </div>
+           
+            
 
           </div>
 
